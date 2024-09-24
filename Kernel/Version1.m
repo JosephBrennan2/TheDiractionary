67f04cfb479b3f31fe4de88d70e9cc7f824fd008
Joseph Brennan <josephb@wolfram.com>
2024-09-24 16:03:38 +0100

    Version 2 Complete

--- Kernel/Version1.m
+++ Kernel/Version1.m
@@ -2,6 +2,7 @@ BeginPackage["TheDiractionary`Version1`", {"TheDiractionary`ScrabbleScore`"}];
 updateUsedCount;
 reduceRemainingCount;
 RunVersion1Scrabblegorithm;
+RunVersion2Scrabblegorithm;
 
 Begin["`Private`"]
 
@@ -29,56 +30,143 @@ reduceRemainingCount[remainingCounts_, word_, blanks_] :=
    	]
   ]
 
-RunVersion1Scrabblegorithm[iterations_] :=
-  Module[{j = 1, dict, wordsByLength, remainingCounts, usedCounts,
-       bingos, blanks, b, word, newRemainingCounts, negativeKeys, i},
-     Do[
-       Print[StringJoin["Attempt ", ToString[j]]];
-       j++;
-       (*Initialize variables*)
-       dict = RandomSample[Import["CSW21.txt", "List"]];
-       wordsByLength = GroupBy[dict, StringLength];
-       wordsByLength[7];
-       remainingCounts = tiles[[All, "Quantity"]];
-       usedCounts = AssociationThread[Keys[tiles], Table[0, 27]];
-       bingos = {};
-       blanks = {};
-       i = 1;
-       (*Main loop.*)
-       While[
-     i <= Length[wordsByLength[7]],
-         word = wordsByLength[7][[i]];
-         If[Length[bingos] < 12, b = 0, b = 1];
-         
+RunVersion1Scrabblegorithm[iterations_] := 
+  Module[{j = 1, dict, wordsByLength, remainingCounts, bingos, blanks,
+     b, word, newRemainingCounts, negativeKeys, i},
+   Do[
+    Print["\n ...\n ...\n ..."];
+    Print[StringJoin["Attempt ", ToString[j]]];
+    j++;
+    (*Initialize variables*)
+    dict = RandomSample[Import["CSW21.txt", "List"]];
+    wordsByLength = GroupBy[dict, StringLength];
+    remainingCounts = tiles[[All, "Quantity"]];
+    bingos = {};
+    blanks = {};
+    i = 1;
+    (*Main loop.*)
+    While[i <= Length[wordsByLength[7]], 
+     word = wordsByLength[7][[i]];
+     If[Length[bingos] < 12, b = 0, b = 1];
      newRemainingCounts = 
       reduceRemainingCount[remainingCounts, word, b];
-         If[newRemainingCounts === remainingCounts, i++,
-           
+     If[newRemainingCounts === remainingCounts, i++, 
       negativeKeys = 
        Select[Keys[newRemainingCounts], newRemainingCounts[#] < 0 &];
-           If[negativeKeys =!= {},
-             AppendTo[blanks, negativeKeys[[1]]];
-             newRemainingCounts[negativeKeys[[1]]] = 0;
-             ];
-           AppendTo[bingos, word];
-           usedCounts = updateUsedCount[usedCounts, word];
-           remainingCounts = newRemainingCounts;
-           If[
-       Length[bingos] == 14,
-       Print["Success"];
+      If[negativeKeys =!= {}, AppendTo[blanks, negativeKeys[[1]]];
+       newRemainingCounts[negativeKeys[[1]]] = 0;];
+      AppendTo[bingos, word];
+      remainingCounts = newRemainingCounts;
+      Print[bingos];
+      Print["Tiles Left: ", 
+       Length[Flatten[KeyValueMap[Table[#1, #2] &, remainingCounts]]],
+        " ", StringJoin[
+        Flatten[KeyValueMap[Table[#1, #2] &, remainingCounts]]]];
+      If[Length[bingos] == 14, Print["Success!"];
        CloudPut[
-        Append[CloudGet["V.1-WordMaster"],
-         <|"Bingos" -> bingos, "Blanks" -> blanks, 
+        Append[CloudGet["V.1-WordMaster"], <|"Bingos" -> bingos, 
+          "Blanks" -> blanks, 
           "Tiles Remaining" -> 
            Flatten[KeyValueMap[Table[#1, #2] &, remainingCounts]]|>], 
-        "V.1-WordMaster"]
-       ];
-           If[Length[bingos] == 12, i = 1, i++]
-           ];
-         ],
-    {iterations}]
-     ];
+        "V.1-WordMaster"]];
+      If[Length[bingos] == 12, i = 1, i++]];], {iterations}]];
 
+
+
+RunVersion2Scrabblegorithm[iterations_] :=
+ Module[
+    {j, dict, wordsByLength, remainingCounts, usedCounts, bingos, blanks, overlaps, blankTileList, usedWords, i,
+   word, b, overlapOptions, overlapTile, newRemainingCounts, charsToDelete},
+  	Do[
+   		Print["\n ...\n ...\n ..."];
+   		Print[StringJoin["Attempt ", ToString[j]]];
+   		j++;
+   		(* Initialize Variables. *)
+   		dict = RandomSample[Import["CSW21.txt", "List"]];
+   		wordsByLength = GroupBy[dict, StringLength];
+   		remainingCounts = tiles[[All, "Quantity"]];
+   		usedCounts = AssociationThread[Keys[tiles], Table[0, 27]];
+   		bingos = {};
+   		blanks = {};
+   		overlaps = {};
+   		blankTileList = {};
+   		usedWords = <||>;(* Track used words *)
+   		i = 1;
+   			(* Select Starting Word. *)
+   			word = RandomChoice[wordsByLength[7]];
+   			remainingCounts = 
+    reduceRemainingCount[remainingCounts, word, 0]
+   			(* If Starting Word requires blanks, skip to next iteration. *);
+   			If[remainingCounts === tiles[[All, "Quantity"]],
+    				Print[word];
+    				Print["Choose another Starter"];
+    				Continue[];
+    			];
+   			usedCounts = updateUsedCount[usedCounts, word];
+   			AppendTo[bingos, word];
+   			Print["Bingos: ", bingos];
+   			Print["Tiles Left: ", Length[Flatten[KeyValueMap[Table[#1, #2] &, remainingCounts]]], 
+    " ", StringJoin[Flatten[KeyValueMap[Table[#1, #2] &, remainingCounts]]]];
+   			Print["Tiles Used: ", Length[Flatten[KeyValueMap[Table[#1, #2] &, usedCounts]]], " ", 
+    StringJoin[Flatten[KeyValueMap[Table[#1, #2] &, usedCounts]]]];
+   			(* Main loop *)
+   			While[i <= Length[wordsByLength[8]],
+    				word = wordsByLength[8][[i]];
+    				If[KeyExistsQ[usedWords, word],
+     					i++;
+     					Continue[];
+     				];
+    				If[Length[bingos] < 12,
+     					b = 0,
+     					b = 1
+     				];
+    				overlapOptions = Intersection[Characters[word], Flatten[KeyValueMap[Table[#1, #2] &, usedCounts]]];
+    				If[overlapOptions === {},
+     					Print[word, " Has No Overlaps!"];
+     					 i++;
+     					 Continue[];
+     				];
+    				overlapTile = RandomChoice[overlapOptions];
+    				newRemainingCounts = reduceRemainingCount[remainingCounts, StringJoin[DeleteElements[Characters[word], 1 -> {overlapTile}]], b];
+    				If[newRemainingCounts === remainingCounts,
+     					i++,
+              blankTileList = Select[Keys[newRemainingCounts], newRemainingCounts[#] < 0 &];
+     					If[blankTileList =!= {},
+      						AppendTo[blanks, blankTileList[[1]]];
+      						newRemainingCounts[blankTileList[[1]]] = 0;
+      						usedCounts["?"]++;
+                  Print[word, " played through: ", overlapTile, " with a blank ", blankTileList[[1]]];,
+      						Print[word, " played through: ", overlapTile]
+      					];
+     					AppendTo[bingos, word];
+     					AppendTo[overlaps, overlapTile];
+     					charsToDelete = Join[{overlapTile}, blankTileList];
+              usedCounts = updateUsedCount[usedCounts, StringJoin[DeleteElements[Characters[word], 1 -> charsToDelete]]];
+     					remainingCounts = newRemainingCounts;
+     					Print[bingos];
+     				  Print["Tiles Left: ", Length[Flatten[KeyValueMap[Table[#1, #2] &, remainingCounts]]], " ", 
+     StringJoin[Flatten[KeyValueMap[Table[#1, #2] &, remainingCounts]]]];
+     					Print["Tiles Used: ", Length[Flatten[KeyValueMap[Table[#1, #2] &, usedCounts]]], " ", 
+      StringJoin[Flatten[KeyValueMap[Table[#1, #2] &, usedCounts]]]];
+     		      usedWords[word] = True;(* Mark the word as used *)
+     					If[Length[bingos] == 14,
+      						Print["Success!"];
+      						CloudPut[Append[CloudGet["V.2-WordMaster"],
+        							<|"Bingos" -> bingos, "Overlaps" -> overlaps, "Blanks" -> blanks,
+         							"Tiles Remaining" -> Flatten[KeyValueMap[Table[#1, #2] &, remainingCounts]]|>],
+       								"V.2-WordMaster"
+       							];
+      						Break[];
+      					];
+     					If[Length[bingos] == Or[12, 13],
+      						i = 1,
+      						i++
+      					];
+     				];
+    			],
+   		{j, 1, iterations}
+   		]
+  	]
 End[]
 
 EndPackage[]
